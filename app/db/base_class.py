de7a12869eaf1dccbcf2f6483e881d0c961f63c7
Francesco Milani <fmilani@bink.com>
2021-07-09 16:31:00 +0300

    BPL-178 add fatabase retry logic

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,7 +1,21 @@
 # mypy checks for sqlalchemy core 2.0 require sqlalchemy2-stubs
+import logging
+
+from typing import TYPE_CHECKING, Any, Callable
+
+import sentry_sdk
+
 from sqlalchemy import Column, DateTime, Integer, text
-from sqlalchemy.orm import declarative_base  # type: ignore
-from sqlalchemy.orm import declarative_mixin  # type: ignore
+from sqlalchemy.exc import DBAPIError
+from sqlalchemy.orm import declarative_base, declarative_mixin  # type: ignore[attr-defined]
+
+from app.core.config import settings
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+    from sqlalchemy.orm import Session
+
+logger = logging.getLogger("db-base-class")
 
 
 class ModelBase:
@@ -22,3 +36,54 @@ class TimestampMixin:
         onupdate=utc_timestamp_sql,
         nullable=False,
     )
+
+
+# based on the following stackoverflow answer:
+# https://stackoverflow.com/a/30004941
+def sync_run_query(
+    fn: Callable,
+    session: "Session",
+    *,
+    attempts: int = settings.DB_CONNECTION_RETRY_TIMES,
+    rollback_on_exc: bool = True,
+    **kwargs: Any,
+) -> Any:  # pragma: no cover
+
+    while attempts > 0:
+        attempts -= 1
+        try:
+            return fn(**kwargs)
+        except DBAPIError as ex:
+            logger.debug(f"Attempt failed: {type(ex).__name__} {ex}")
+            if rollback_on_exc:
+                session.rollback()
+
+            if attempts > 0 and ex.connection_invalidated:
+                logger.warning(f"Interrupted transaction: {repr(ex)}, attempts remaining:{attempts}")
+            else:
+                sentry_sdk.capture_message(f"Max db connection attempts reached: {repr(ex)}")
+                raise
+
+
+async def async_run_query(
+    fn: Callable,
+    session: "AsyncSession",
+    *,
+    attempts: int = settings.DB_CONNECTION_RETRY_TIMES,
+    rollback_on_exc: bool = True,
+    **kwargs: Any,
+) -> Any:  # pragma: no cover
+    while attempts > 0:
+        attempts -= 1
+        try:
+            return await fn(**kwargs)
+        except DBAPIError as ex:
+            logger.debug(f"Attempt failed: {type(ex).__name__} {ex}")
+            if rollback_on_exc:
+                await session.rollback()
+
+            if attempts > 0 and ex.connection_invalidated:
+                logger.warning(f"Interrupted transaction: {repr(ex)}, attempts remaining:{attempts}")
+            else:
+                sentry_sdk.capture_message(f"Max db connection attempts reached: {repr(ex)}")
+                raise
