f9464b802f0916674ffa0c6ea2793a9470c88c27
David Felce <dfelce@bink.com>
2021-07-13 14:00:17 +0100

    BPL-110: added voucher table

--- app/models/voucher.py
+++ app/models/voucher.py
@@ -1,6 +1,22 @@
-from sqlalchemy import Column, Integer, String, UniqueConstraint
+import uuid
+
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, UniqueConstraint
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base, TimestampMixin
+
+
+class Voucher(Base, TimestampMixin):
+    __tablename__ = "voucher"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    voucher_code = Column(String, nullable=False, unique=True, index=True)
+    allocated = Column(Boolean, default=False, nullable=False)
+    voucher_config_id = Column(Integer, ForeignKey("voucher_config.id", ondelete="CASCADE"), nullable=False)
+    voucher_config = relationship("VoucherConfig", back_populates="vouchers")
+
+    __mapper_args__ = {"eager_defaults": True}
 
 
 class VoucherConfig(Base, TimestampMixin):
@@ -9,6 +25,7 @@ class VoucherConfig(Base, TimestampMixin):
     voucher_type_slug = Column(String(32), index=True, nullable=False)
     validity_days = Column(Integer, nullable=True)
     retailer_slug = Column(String(32), index=True, nullable=False)
+    vouchers = relationship("Voucher", back_populates="voucher_config", lazy="joined")
 
     __mapper_args__ = {"eager_defaults": True}
     __table_args__ = (
