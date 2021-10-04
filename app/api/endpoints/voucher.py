a5b74c917cfe95d9f8b97d1530de707dd233c9f7
Stewart Perrygrove <sperrygrove@bink.com>
2021-10-04 19:10:30 +0100

    BPL-275 End point to alter voucher type status

--- app/api/endpoints/voucher.py
+++ app/api/endpoints/voucher.py
@@ -7,8 +7,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app import crud
 from app.api.deps import get_session, user_is_authorised
+from app.db.base_class import async_run_query
+from app.enums import VoucherTypeStatuses
 from app.fetch_voucher import get_allocable_voucher
 from app.schemas import VoucherAllocationSchema
+from app.schemas.voucher import VoucherStatusSchema
 from app.tasks.voucher import enqueue_voucher_allocation
 
 router = APIRouter()
@@ -32,4 +35,26 @@ async def allocation(
     )
 
     asyncio.create_task(enqueue_voucher_allocation(voucher_allocation.id))
+    return {}
+
+
+@router.patch(
+    path="/{retailer_slug}/vouchers/{voucher_type_slug}/status",
+    status_code=status.HTTP_202_ACCEPTED,
+    dependencies=[Depends(user_is_authorised)],
+)
+async def voucher_type_status(
+    payload: VoucherStatusSchema,
+    retailer_slug: str,
+    voucher_type_slug: str,
+    db_session: AsyncSession = Depends(get_session),
+) -> Any:
+    voucher_config = await crud.get_voucher_config(db_session, retailer_slug, voucher_type_slug, for_update=True)
+    voucher_config.status = VoucherTypeStatuses(payload.status)
+
+    async def _query() -> None:
+        return await db_session.commit()
+
+    await async_run_query(_query, db_session)
+    # placeholder - add task to delete vouchers here
     return {}
