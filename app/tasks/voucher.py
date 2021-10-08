4a30fb1076139438225920198c88b3c373d75637
Francesco Milani <fmilani@bink.com>
2021-10-08 16:28:35 +0300

    BPL-265 correct typo

--- app/tasks/voucher.py
+++ app/tasks/voucher.py
@@ -9,7 +9,7 @@ async def enqueue_voucher_allocation_retry_task(retry_task_id: int) -> None:  # 
 
     async with AsyncSessionMaker() as db_session:
         await enqueue_retry_task(
-            async_db_session=db_session,
+            db_session=db_session,
             retry_task_id=retry_task_id,
             action=issue_voucher,
             queue=settings.VOUCHER_ALLOCATION_TASK_QUEUE,
