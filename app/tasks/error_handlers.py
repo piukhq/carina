f2201b8d84378f792fe304c19c4084351d510542
Francesco Milani <fmilani@bink.com>
2021-11-30 14:32:49 +0200

    BPL-187 delete and cancel vouchers tasks

--- app/tasks/error_handlers.py
+++ app/tasks/error_handlers.py
@@ -1,20 +1,42 @@
 # this file is excluded from coverage as there is no logic to test here beyond calling a library function.
 # if in the future we add any logic worth testing, please remove this file from the coveragerc ignore list.
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Callable
 
 import rq
+import sentry_sdk
 
 from retry_tasks_lib.utils.error_handler import handle_request_exception
 
 from app.core.config import redis, settings
 from app.db.session import SyncSessionMaker
 
+from . import logger
+
 if TYPE_CHECKING:
     from inspect import Traceback
 
 
+def log_internal_exception(func: Callable) -> Any:
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        try:
+            return func(*args, **kwargs)
+        except Exception as ex:
+            logger.exception(ex)
+            sentry_sdk.capture_exception(ex)
+            raise
+
+    return wrapper
+
+
+def default_handler(
+    job: rq.job.Job, exc_type: type, exc_value: Exception, traceback: "Traceback"
+) -> Any:  # pragma: no cover
+    return True  # defer to the RQ default handler
+
+
 # NOTE: Inter-dependency: If this function's name or module changes, ensure that
 # it is relevantly reflected in the TaskType table
+@log_internal_exception
 def handle_retry_task_request_error(
     job: rq.job.Job, exc_type: type, exc_value: Exception, traceback: "Traceback"
 ) -> None:
