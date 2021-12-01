b8238fc243728b2535e420b84c24d160d69bfe44
Francesco Milani <fmilani@bink.com>
2021-12-01 17:12:05 +0200

    BPL-187 post rebase migration conflict fix

--- alembic/versions/4282380cb010_cancel_vouchers_task_type.py
+++ alembic/versions/4282380cb010_cancel_vouchers_task_type.py
@@ -11,7 +11,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "4282380cb010"
-down_revision = "15d687a1f3f2"
+down_revision = "f96899b57452"
 branch_labels = None
 depends_on = None
 
