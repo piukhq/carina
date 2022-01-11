a053d5f0c018d866956f56b19a04eff37fe5e0c3
Haffi Mazhar <haffimazhar96@gmail.com>
2022-01-11 15:25:16 +0000

    update polaris endpoint rename

--- app/tasks/voucher_cancellation.py
+++ app/tasks/voucher_cancellation.py
@@ -10,38 +10,38 @@ from . import logger, send_request_with_metrics
 from .prometheus import tasks_run_total
 
 
-def _process_vouchers_cancellation(task_params: dict) -> dict:
-    logger.info(f"Processing vouchers' cancellation for voucher type: {task_params['voucher_type_slug']}")
+def _process_rewards_cancellation(task_params: dict) -> dict:
+    logger.info(f"Processing rewards' cancellation for voucher type: {task_params['reward_type_slug']}")
     timestamp = datetime.utcnow()
     response_audit: dict = {"timestamp": timestamp.isoformat()}
 
     resp = send_request_with_metrics(
         "POST",
-        "{base_url}/bpl/loyalty/{retailer_slug}/vouchers/{voucher_type_slug}/cancel".format(
+        "{base_url}/bpl/loyalty/{retailer_slug}/rewards/{reward_type_slug}/cancel".format(
             base_url=settings.POLARIS_URL,
             retailer_slug=task_params["retailer_slug"],
-            voucher_type_slug=task_params["voucher_type_slug"],
+            reward_type_slug=task_params["reward_type_slug"],
         ),
         headers={"Authorization": f"Token {settings.POLARIS_AUTH_TOKEN}"},
         timeout=(3.03, 10),
     )
     resp.raise_for_status()
     response_audit["response"] = {"status": resp.status_code, "body": resp.text}
-    logger.info(f"Vouchers' cancellation succeeded for voucher type: {task_params['voucher_type_slug']}")
+    logger.info(f"Rewards' cancellation succeeded for voucher type: {task_params['reward_type_slug']}")
 
     return response_audit
 
 
 # NOTE: Inter-dependency: If this function's name or module changes, ensure that
 # it is relevantly reflected in the TaskType table
-def cancel_vouchers(retry_task_id: int) -> None:
+def cancel_rewards(retry_task_id: int) -> None:
     tasks_run_total.labels(app=settings.PROJECT_NAME, task_name=settings.CANCEL_VOUCHERS_TASK_NAME).inc()
     with SyncSessionMaker() as db_session:
 
         retry_task = get_retry_task(db_session, retry_task_id)
         retry_task.update_task(db_session, increase_attempts=True)
 
-        response_audit = _process_vouchers_cancellation(retry_task.get_params())
+        response_audit = _process_rewards_cancellation(retry_task.get_params())
 
         retry_task.update_task(
             db_session, response_audit=response_audit, status=RetryTaskStatuses.SUCCESS, clear_next_attempt_time=True
