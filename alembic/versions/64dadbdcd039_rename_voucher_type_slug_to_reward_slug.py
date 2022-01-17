4ca002ddfb971697cad899a5b629b42a19bcd604
Haffi Mazhar <haffimazhar96@gmail.com>
2022-01-17 15:37:10 +0000

    Testing branch - Both migrations in one

--- alembic/versions/64dadbdcd039_rename_voucher_type_slug_to_reward_slug.py
+++ alembic/versions/64dadbdcd039_rename_voucher_type_slug_to_reward_slug.py
@@ -26,6 +26,15 @@ def upgrade():
     # Update task_type_key.name for voucher_type_slug
     op.execute("UPDATE task_type_key SET name = 'reward_slug' from task_type where task_type.name = 'cancel-rewards' and task_type_key.task_type_id = task_type.task_type_id")
 
+    # Update task_type.name to delete-unallocated-rewards
+    op.execute(
+        "UPDATE task_type SET path = 'app.tasks.reward_deletion.delete_unallocated_rewards', name = 'delete-unallocated-rewards' "
+        "WHERE name = 'delete-unallocated-vouchers' AND path = 'app.tasks.voucher_deletion.delete_unallocated_vouchers'"
+    )
+
+    # Update task_type_key.name for voucher_type_slug
+    op.execute("UPDATE task_type_key SET name = 'reward_slug' from task_type where task_type.name = 'delete-unallocated-rewards' and task_type_key.task_type_id = task_type.task_type_id")
+
 
 
 
@@ -36,6 +45,16 @@ def downgrade():
         "WHERE name ='cancel-rewards' AND path = 'app.tasks.reward_cancellation.cancel_rewards'"
     )
 
-    # # Revert task_type_key.name for reward_slug
+    # Revert task_type_key.name for reward_slug
     op.execute("UPDATE task_type_key SET name = 'voucher_type_slug' from task_type where task_type.name = 'cancel-vouchers' and task_type_key.task_type_id = task_type.task_type_id")
+
+
+    # Revert task_type.name to delete-unallocated-vouchers
+    op.execute(
+        "UPDATE task_type SET path = 'app.tasks.voucher_deletion.delete_unallocated_vouchers', name = 'delete-unallocated-vouchers' "
+        "WHERE name ='delete-unallocated-rewards' AND path = 'app.tasks.reward_deletion.delete_unallocated_rewards'"
+    )
+
+    # Revert task_type_key.name for reward_slug
+    op.execute("UPDATE task_type_key SET name = 'voucher_type_slug' from task_type where task_type.name = 'delete-unallocated-rewards' and task_type_key.task_type_id = task_type.task_type_id")
 
