a30417acecb1528b9c87d052e82004bf03a88815
Haffi Mazhar <haffimazhar96@gmail.com>
2022-01-21 16:45:56 +0000

    alembic - cleanup. Migration working

--- alembic/versions/69cc1d0099e4_rename_voucher_tables_to_reward.py
+++ alembic/versions/69cc1d0099e4_rename_voucher_tables_to_reward.py
@@ -20,13 +20,7 @@ def upgrade():
     conn = op.get_bind()
     existing_metadata = sa.schema.MetaData()
 
-    ### 1st table - Voucher
-    # Need to rename columns:
-    # id: Need to drop PK
-    # voucher_code -> code
-    # voucher_config_id -> reward_config_id, need to drop FK 
-    # reward_config relationship
-    # Renaming constraints
+    ## 1st table - Voucher
 
     new_reward_table = "reward"
     old_voucher_table = "voucher"
@@ -39,7 +33,6 @@ def upgrade():
 
     # rename tables
     op.rename_table(old_voucher_table, new_reward_table)
-    op.rename_table("voucher_config", "reward_config")
 
     # Drop referenced FK from voucher_update table
     op.drop_constraint("voucher_update_voucher_id_fkey", "voucher_update") # voucher_update table
@@ -54,8 +47,7 @@ def upgrade():
     op.alter_column(table_name=new_reward_table, column_name="voucher_config_id", new_column_name="reward_config_id")
 
     # Re-create FK and PK
-    # referent_table = "reward_config" ##TODO: This depends on voucher_config table rename
-    # op.create_foreign_key(f"{new_reward_table}_reward_config_id_fkey", "reward", "reward_config", ["reward_config_id"], ["id"])
+    # FK created during 2nd table migration due to dependency, L: 80
     op.create_primary_key(f"{new_reward_table}_pkey", new_reward_table, ["id"])
 
     # Re-create indices
@@ -65,25 +57,19 @@ def upgrade():
     op.create_index("ix_reward_code", new_reward_table, ["code"])
 
 
-    ### 2nd table - VoucherConfig
-    # Need to rename columns:
-    # id: Need to drop PK
-    # voucher_type_slug -> reward_slug
-    # Fetch type and status has enums which are renamed
-    # rewards relationship
-    # Renaming constraints
+    ## 2nd table - VoucherConfig
 
     old_voucher_config_table = "voucher_config"
     new_reward_config_table = "reward_config"
+
+    # renamed voucher_config -> reward_config
+    op.rename_table("voucher_config", "reward_config")
 
     # Drop indices
     for index in ("ix_voucher_config_retailer_slug", "ix_voucher_config_voucher_type_slug"):
         op.drop_index(op.f(index), table_name=old_voucher_config_table)
 
     op.drop_constraint("voucher_type_slug_retailer_slug_unq", table_name=new_reward_config_table)
-    
-    # rename voucher_config table -> reward_config
-    # op.rename_table(old_voucher_config_table, new_reward_config_table) Already done in 1st table
 
     # Drop PK from existing table
     existing_table = sa.Table(new_reward_config_table, existing_metadata, autoload_with=conn)
@@ -91,7 +77,7 @@ def upgrade():
 
     # Re-create PK
     op.create_primary_key(f"{new_reward_config_table}_pkey", new_reward_config_table, ["id"])
-    op.create_foreign_key(f"{new_reward_table}_reward_config_id_fkey", "reward", "reward_config", ["reward_config_id"], ["id"])
+    op.create_foreign_key(f"{new_reward_table}_reward_config_id_fkey", new_reward_table, new_reward_config_table, ["reward_config_id"], ["id"])
   
     # Alter sequence
     op.execute(f"ALTER SEQUENCE {old_voucher_config_table}_id_seq RENAME TO {new_reward_config_table}_id_seq;")
@@ -109,13 +95,7 @@ def upgrade():
     op.create_index("ix_reward_config_reward_slug", new_reward_config_table, ["reward_slug"])
 
     
-    # 3rd table - VoucherUpdate
-
-    # Need to rename columns:
-    # id: Need to drop PK
-    # voucher_id -> reward_uuid, need to drop FK and rename
-    # reward relationship
-    # No unique constraints
+    ## 3rd table - VoucherUpdate
 
     old_voucher_update_table = "voucher_update"
     new_reward_update_table = "reward_update"
@@ -123,16 +103,16 @@ def upgrade():
     # rename voucher_config table -> reward_config
     op.rename_table(old_voucher_update_table, new_reward_update_table)
 
-    # Drop PK from existing table
+    # Drop PK and FK from existing table
     existing_table = sa.Table(new_reward_update_table, existing_metadata, autoload_with=conn)
     op.drop_constraint(existing_table.primary_key.name, new_reward_update_table)
-    # op.drop_constraint(existing_table.foreign_key_constraints, new_reward_update_table)
+    # FK dropped earlier due to dependency in voucher table, L: 38
 
     # Alter columns
     op.alter_column(table_name=new_reward_update_table, column_name="voucher_id", new_column_name="reward_uuid")    
 
     # Re-create FK and PK
-    referent_reward_table = "reward" ##TODO: This depends on voucher table rename
+    referent_reward_table = "reward"
     op.create_foreign_key(f"{new_reward_update_table}_reward_uuid_fkey", new_reward_update_table, referent_reward_table, ["reward_uuid"], ["id"], ondelete="CASCADE",)
     op.create_primary_key(f"{new_reward_update_table}_pkey", new_reward_update_table, ["id"])
 
@@ -143,16 +123,12 @@ def upgrade():
 
     ## 4th table - VoucherFileLog
 
-    # id: Need to drop PK
-    # unique constraints rename
-
     old_voucher_file_log_table = "voucher_file_log"
     new_reward_file_log_table = "reward_file_log"
 
     # Drop indices
     for index in (f"ix_{old_voucher_file_log_table}_file_name", f"ix_{old_voucher_file_log_table}_file_agent_type"):
         op.drop_index(op.f(index), table_name=old_voucher_file_log_table)
-    # op.drop_constraint("file_name_file_agent_type_unq", table_name=old_voucher_file_log_table)
 
     # rename voucher_file_log table -> reward_file_log
     op.rename_table(old_voucher_file_log_table, new_reward_file_log_table)
@@ -165,7 +141,6 @@ def upgrade():
     op.create_primary_key(f"{new_reward_file_log_table}_pkey", new_reward_file_log_table, ["id"])
 
     # Re-create indices
-    # op.create_unique_constraint("file_name_file_agent_type_unq", new_reward_file_log_table, ["file_name", "file_agent_type"])
     op.create_index(f"ix_{new_reward_file_log_table}_file_name", new_reward_file_log_table, ["file_name"])
     op.create_index(f"ix_{new_reward_file_log_table}_file_agent_type", new_reward_file_log_table, ["file_agent_type"])
 
@@ -192,7 +167,6 @@ def downgrade():
 
     # rename tables
     op.rename_table(old_reward_table, new_voucher_table)
-    op.rename_table("reward_config", "voucher_config")
 
     # Drop referenced FK from voucher_update table
     op.drop_constraint("reward_update_reward_uuid_fkey", "reward_update") # voucher_update table
@@ -207,12 +181,8 @@ def downgrade():
     op.alter_column(table_name=new_voucher_table, column_name="reward_config_id", new_column_name="voucher_config_id")
 
     # Re-create FK and PK
-    referent_table = "voucher_config" ##TODO: This depends on voucher_config table rename
-    # op.create_foreign_key(f"{new_voucher_table}_voucher_config_id_fkey", new_voucher_table, referent_table, ["voucher_config_id"], ["id"])
+    # FK created during 2nd table migration due to dependency, L: 218
     op.create_primary_key(f"{new_voucher_table}_pkey", new_voucher_table, ["id"])
-
-    # # Create referenced FK from voucher_update table
-    # op.create_foreign_key("voucher_update_voucher_id_fkey", "voucher_update", "voucher", ["voucher_id"], ["id"], ondelete="CASCADE") # reward_update table
 
     # Re-create indices
     op.create_unique_constraint("voucher_code_retailer_slug_voucher_config_unq", new_voucher_table, ["voucher_code", "retailer_slug", "voucher_config_id"])
@@ -227,20 +197,21 @@ def downgrade():
     old_reward_config_table = "reward_config"
     new_voucher_config_table = "voucher_config"
 
+    # rename voucher_config table -> reward_config
+    op.rename_table(old_reward_config_table, new_voucher_config_table)
+
     # Drop indices
     for index in ("ix_reward_config_retailer_slug", "ix_reward_config_reward_slug"):
         op.drop_index(op.f(index), table_name=old_reward_config_table)
 
     op.drop_constraint("reward_slug_retailer_slug_unq", table_name=new_voucher_config_table)
-    
-    # rename voucher_config table -> reward_config
-    # op.rename_table(old_reward_config_table, new_voucher_config_table)
 
     # Drop PK from existing table
     existing_table = sa.Table(new_voucher_config_table, existing_metadata, autoload_with=conn)
     op.drop_constraint(existing_table.primary_key.name, new_voucher_config_table)
 
     # Re-create PK
+    referent_table = "voucher_config"
     op.create_primary_key(f"{new_voucher_config_table}_pkey", new_voucher_config_table, ["id"])
     op.create_foreign_key(f"{new_voucher_table}_voucher_config_id_fkey", new_voucher_table, referent_table, ["voucher_config_id"], ["id"])
 
@@ -268,10 +239,10 @@ def downgrade():
     # rename voucher_config table -> reward_config
     op.rename_table(old_reward_update_table, new_voucher_update_table)
 
-    # Drop PK from existing table
+    # Drop PK and FK from existing table
     existing_table = sa.Table(new_voucher_update_table, existing_metadata, autoload_with=conn)
     op.drop_constraint(existing_table.primary_key.name, new_voucher_update_table)
-    # op.drop_constraint(existing_table.foreign_key_constraints, new_voucher_update_table)
+    # FK dropped earlier due to dependency in reward table, L: 172
 
      # Alter columns
     op.alter_column(table_name=new_voucher_update_table, column_name="reward_uuid", new_column_name="voucher_id")    
@@ -294,7 +265,6 @@ def downgrade():
     # Drop indices
     for index in (f"ix_{old_reward_file_log_table}_file_name", f"ix_{old_reward_file_log_table}_file_agent_type"):
         op.drop_index(op.f(index), table_name=old_reward_file_log_table)
-    # op.drop_constraint("file_name_file_agent_type_unq", table_name=old_voucher_file_log_table)
 
     # rename voucher_file_log table -> reward_file_log
     op.rename_table(old_reward_file_log_table, new_voucher_file_log_table)
@@ -307,7 +277,6 @@ def downgrade():
     op.create_primary_key(f"{new_voucher_file_log_table}_pkey", new_voucher_file_log_table, ["id"])
 
     # Re-create indices
-    # op.create_unique_constraint("file_name_file_agent_type_unq", new_reward_file_log_table, ["file_name", "file_agent_type"])
     op.create_index(f"ix_{new_voucher_file_log_table}_file_name", new_voucher_file_log_table, ["file_name"])
     op.create_index(f"ix_{new_voucher_file_log_table}_file_agent_type", new_voucher_file_log_table, ["file_agent_type"])
 
