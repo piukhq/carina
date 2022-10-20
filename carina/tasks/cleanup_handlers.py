1bb07b6542793e8a8ced4b03b06712c99a3e3970
Francesco Milani <fmilani@bink.com>
2022-10-20 14:56:43 +0300

    BPL-797 fix fetching of reward config in cleanup handler

--- carina/tasks/cleanup_handlers.py
+++ carina/tasks/cleanup_handlers.py
@@ -2,12 +2,10 @@ from typing import TYPE_CHECKING
 
 from retry_tasks_lib.db.models import RetryTask
 from retry_tasks_lib.utils.synchronous import cleanup_handler
-from sqlalchemy.future import select
 
-from carina.db.base_class import sync_run_query
 from carina.db.session import SyncSessionMaker
 from carina.fetch_reward import cleanup_reward
-from carina.models import RewardConfig
+from carina.tasks.shared_crud import get_reward_config
 
 if TYPE_CHECKING:
     from sqlalchemy.orm import Session
@@ -15,10 +13,5 @@ if TYPE_CHECKING:
 
 @cleanup_handler(db_session_factory=SyncSessionMaker)
 def reward_issuance_cleanup_handler(retry_task: RetryTask, db_session: "Session") -> None:
-    reward_config_id: int = retry_task.get_params()["reward_config_id"]
-    reward_config: RewardConfig = sync_run_query(
-        lambda: db_session.execute(select(RewardConfig).where(RewardConfig.id == reward_config_id)),
-        db_session,
-        rollback_on_exc=False,
-    )
+    reward_config = get_reward_config(db_session, retry_task.get_params()["reward_config_id"])
     cleanup_reward(db_session, reward_config, retry_task)
