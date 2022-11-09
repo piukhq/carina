e532c1ffc1256f967703b36ef747554ef5cda45b
Haffi Mazhar <hmazhar@bink.com>
2022-11-09 10:48:50 +0000

    BPL-830: Remove campaign slug validator

--- tests/functional/test_schemas.py
+++ tests/functional/test_schemas.py
@@ -4,13 +4,6 @@ from carina.enums import RewardCampaignStatuses
 from carina.schemas import RewardCampaignSchema
 
 
-def test_reward_campaign_schema() -> None:
-    reward_campaign_payload = RewardCampaignSchema(
-        campaign_slug="  Campaign-Slug  ", status=RewardCampaignStatuses.ACTIVE
-    )
-    assert reward_campaign_payload.campaign_slug == "campaign-slug"
-
-
 def test_reward_campaign_schema_raises() -> None:
     with pytest.raises(ValueError):
         RewardCampaignSchema(campaign_slug="   ", status=RewardCampaignStatuses.ACTIVE)
